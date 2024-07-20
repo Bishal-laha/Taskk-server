55cd005be8e2e61172fdf5054fb87fbd0471e806
Bishal-laha <bishallaha12345@gmail.com>
2024-07-20 15:59:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import { app } from "./app.js";
 
 dotenv.config({
-    path:'./.env'
+    path:'../.env'
 });
 
 app.listen(process.env.PORT_NUMBER || 8000, ()=>{console.log(`SERVER IS RUNNING ON ${process.env.PORT_NUMBER}`)});
